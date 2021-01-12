1db1cfe57d75773b50e89c3942d534f61f67cc76
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2021-01-12 02:23:28 -0600

    completed restaurant menu studio

--- src/org/launchcode/java/studios/restaurantmenu/Menu.java
+++ src/org/launchcode/java/studios/restaurantmenu/Menu.java
@@ -1,2 +1,25 @@
-package org.launchcode.java.studios.restaurantmenu;public class Menu {
+package org.launchcode.java.studios.restaurantmenu;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+
+    //ClassVariables
+    //arrayList
+    private ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+    private Date lastUpdated;
+
+    //constructors
+    public Menu() {
+        this.lastUpdated = new Date();
+    }
+
+    //methods
+    //getters and setters
+    public ArrayList<MenuItem> getMenuItems() { return this.menuItems; }
+    public void setMenuItems(ArrayList<MenuItem> menuItems) { this.menuItems = menuItems}
+
+    public Date getLastUpdated() { return this.lastUpdated; }
+    public void setLastUpdated(Date lastUpdated) { this.lastUpdated = lastUpdated; }
 }
